f4b03ae030fc4e9ff836d19deab3c4dd1c65a608
NicolasRasse <35576430+NicolasRasse@users.noreply.github.com>
2019-04-30 21:56:14 -0300

    ver 01

---
+++ index.js
@@ -0,0 +1,22 @@
+const TelegramBot = require('node-telegram-bot-api');
+ 
+
+const token = '892564806:AAFpp6qN2ozzZk-40pJp5wo8DgglyO6yEWA';
+ 
+
+const bot = new TelegramBot(token, {polling: true});
+ 
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+ 
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+ 
+  bot.sendMessage(chatId, 'Received your message');
+});
