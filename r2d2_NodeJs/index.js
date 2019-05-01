02b59934b1bbe6e47a0801345764940a558925a9
NicolasRasse <35576430+NicolasRasse@users.noreply.github.com>
2019-04-30 22:44:31 -0300

    ver 03

--- r2d2_NodeJs/index.js
+++ r2d2_NodeJs/index.js
@@ -1,48 +1,17 @@
-const TelegramBot = require('bluebird');
+const TelegramBot = require('node-telegram-bot-api');
 
-const token = '892564806:AAFpp6qN2ozzZk-40pJp5wo8DgglyO6yEWA';//Cambiar por el token de telegram
-const bot = new TelegramBot(token, {
-  polling: true
-});
-var IdMiChat = 292687930;//cambiar por tu ID del chat
+const token = '892564806:AAFpp6qN2ozzZk-40pJp5wo8DgglyO6yEWA';
 
-var SerialPort = require('serialport');
-var MiPuerto = new SerialPort('COM3', {
-  baudRate: 9600,
-  autoOpen: true
-});
-
-//aca va el IF
+const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', (msg) => {
-
-
- const chatId = msg.chat.id;
-  console.log("El ID del char" + chatId);
+  const chatId = msg.chat.id;
   var Mensaje = msg.text;
-    var pideping = msg.text;
-  if (pideping == "1234"){
-
-     if (Mensaje == "Encender") {
-    console.log("encendiendo el led");
-    bot.sendMessage(chatId, 'Encendere el led');
-    MiPuerto.write("H");
-    } else if (Mensaje == "Apagar") {
-    console.log("apagar el led");
-    bot.sendMessage(chatId, 'Apagare el led');
-    MiPuerto.write("L");
+  if (Mensaje == "encender"){
+    console.log("encendiendo el Led");
   }
-} else {
-bot.sendMessage(chatId, 'Usted no posee permisos.');
-};
-
-MiPuerto.setEncoding('utf8');
-
-MiPuerto.on('data', function(data) {
-  console.log("Lo que entro es " + data);
-  if (data[0] == 'H') {
-    console.log("Boton Precionado");
-    bot.sendMessage(IdMiChat, "Se preciono el boton");
+  else if (Mensaje == "apagar") {
+    console.log("apagar el Led");
   }
-
+  bot.sendMessage(chatId, 'Received your message');
 });
