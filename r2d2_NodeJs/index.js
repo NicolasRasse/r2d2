d8433b5b287daa70aec402ee39522762ca717f98
NicolasRasse <35576430+NicolasRasse@users.noreply.github.com>
2019-05-01 17:51:32 -0300

    paso uno terminado

--- r2d2_NodeJs/index.js
+++ r2d2_NodeJs/index.js
@@ -1,40 +1,38 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '892564806:AAFpp6qN2ozzZk-40pJp5wo8DgglyO6yEWA';
-
-const bot = new TelegramBot(token, {polling: true});
-
-var IdMiChat = 292687930;
+const token = '892564806:AAFpp6qN2ozzZk-40pJp5wo8DgglyO6yEWA';//Cambiar por el token de telegram
+const bot = new TelegramBot(token, {
+  polling: true
+});
+var IdMiChat = 292687930;//cambiar por tu ID del chat
 
 var SerialPort = require('serialport');
-var MiPuerto = new SerialPort('COM3',
-{baudRate: 9600,
- autoOpen: true
+var MiPuerto = new SerialPort('COM3', {
+  baudRate: 9600,
+  autoOpen: true
 });
 
-
 bot.on('message', (msg) => {
-        const chatId = msg.chat.id;
-        console.log("El ID del chat" + chatId);
-        var Mensaje = msg.text;
-        if (Mensaje == "encender"){
-          console.log("encendiendo el Led");
-          bot.sendMessage(chatId, 'Encendiendo La Luz Del Patio');
-          MiPuerto.write("H");
-        }
-        else if (Mensaje == "apagar") {
-          console.log("apagar el Led");
-          bot.sendMessage(chatId, 'Apagando La Luz Del Patio');
-          MiPuerto.wirte("L");
-        }
-      });
+  const chatId = msg.chat.id;
+  console.log("El ID del char " + chatId);
+  var Mensaje = msg.text;
+  if (Mensaje == "Encender") {
+    console.log("encendiendo el led");
+    bot.sendMessage(chatId, 'Encendere el led');
+    MiPuerto.write("H");
+  } else if (Mensaje == "Apagar") {
+    console.log("apagar el led");
+    bot.sendMessage(chatId, 'Apagare el led');
+    MiPuerto.write("L");
+  }
+});
 
-  MiPuerto.setEncoding('utf8');
+MiPuerto.setEncoding('utf8');
 
-  MiPuerto.on('data', function('data') {
-    console.log("Lo que entro es " + data);
-    if (data[0] == 'H') {
-      console.log("Boton Precionado");
-      bot.sendMessage(IdMiChat, "Se presiono el boton");
-    }
-  });
+MiPuerto.on('data', function(data) {
+  console.log("Lo que entro es " + data);
+  if (data[0] == 'H') {
+    console.log("Boton Precionado");
+    bot.sendMessage(IdMiChat, "Se preciono el boton");
+  }
+});
