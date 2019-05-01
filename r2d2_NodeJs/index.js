18938bbf2243adf9b9a42da39f59ae181f980daa
NicolasRasse <35576430+NicolasRasse@users.noreply.github.com>
2019-04-30 23:38:12 -0300

    ver 05

--- r2d2_NodeJs/index.js
+++ r2d2_NodeJs/index.js
@@ -4,16 +4,22 @@ const token = '892564806:AAFpp6qN2ozzZk-40pJp5wo8DgglyO6yEWA';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  var Mensaje = msg.text;
-  if (Mensaje == "encender"){
-    console.log("encendiendo el Led");
-    bot.sendMessage(chatId, 'Encendiendo La Luz Del Patio');
-  }
-  else if (Mensaje == "apagar") {
-    console.log("apagar el Led");
-    bot.sendMessage(chatId, 'Apagando La Luz Del Patio');
-  }
-
+var SerialPort = require('serialport');
+var port = new SerailPort('COM3',
+{baudRate: 9600
 });
+
+
+bot.on('message', (msg) => {
+        const chatId = msg.chat.id;
+        var Mensaje = msg.text;
+        if (Mensaje == "encender"){
+          console.log("encendiendo el Led");
+          bot.sendMessage(chatId, 'Encendiendo La Luz Del Patio');
+        }
+        else if (Mensaje == "apagar") {
+          console.log("apagar el Led");
+          bot.sendMessage(chatId, 'Apagando La Luz Del Patio');
+        }
+
+      });
